d22f631af6ecbf43ca769987297a2211c1180141
akorkot <ayoub.korkot@gmail.com>
2017-02-06 23:08:35 +0000

    Les controleurs sous symfony

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -4,53 +4,96 @@
 
 namespace OC\PlatformBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
 {
-
 	/**
-	 * @return Response
-	 * @throws \Twig_Error
+	 * @param $page
+	 * @return \Symfony\Component\HttpFoundation\Response
 	 */
-    public function indexAction()
-    {
+	public function indexAction($page)
+	{
+		// On ne sait pas combien de pages il y a
+		// Mais on sait qu'une page doit être supérieure ou égale à 1
+		if ($page < 1) {
+			// On déclenche une exception NotFoundHttpException, cela va afficher
+			// une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
+			throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+		}
 
-        $content = $this
-			->get('templating')
-			->render('OCPlatformBundle:Advert:index.html.twig', array(
-				"name" => "Korkot"
-			)
-		);
-		return new Response($content);
-    }
+		// Ici, on récupérera la liste des annonces, puis on la passera au template
+
+		// Mais pour l'instant, on ne fait qu'appeler le template
+		return $this->render('OCPlatformBundle:Advert:index.html.twig');
+	}
 
 	/**
 	 * @param $id
-	 * @return Response
+	 * @return \Symfony\Component\HttpFoundation\Response
 	 */
 	public function viewAction($id)
 	{
-        $url = $this->generateUrl(
-            'oc_platform_view',
-            array('id' => $id),
-            true
-        );
+		// Ici, on récupérera l'annonce correspondante à l'id $id
 
-        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
+		return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+			'id' => $id
+		));
 	}
 
-    /**
-     * @param $id
-     * @return Response
-     */
-    public function viewSlugAction($slug, $year, $format)
-    {
-        return new Response(
-            "On pourrait afficher l'annonce correspondant au
-            slug '".$slug."', créée en ".$year." et au format ".$format."."
-        );
-    }
+	/**
+	 * @param Request $request
+	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+	 */
+	public function addAction(Request $request)
+	{
+		// La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+
+		// Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+		if ($request->isMethod('POST')) {
+			// Ici, on s'occupera de la création et de la gestion du formulaire
+
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+			// Puis on redirige vers la page de visualisation de cettte annonce
+			return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+		}
+
+		// Si on n'est pas en POST, alors on affiche le formulaire
+		return $this->render('OCPlatformBundle:Advert:add.html.twig');
+	}
+
+	/**
+	 * @param $id
+	 * @param Request $request
+	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+	 */
+	public function editAction($id, Request $request)
+	{
+		// Ici, on récupérera l'annonce correspondante à $id
+
+		// Même mécanisme que pour l'ajout
+		if ($request->isMethod('POST')) {
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+
+			return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+		}
+
+		return $this->render('OCPlatformBundle:Advert:edit.html.twig');
+	}
+
+	/**
+	 * @param $id
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	public function deleteAction($id)
+	{
+		// Ici, on récupérera l'annonce correspondant à $id
+
+		// Ici, on gérera la suppression de l'annonce en question
+
+		return $this->render('OCPlatformBundle:Advert:delete.html.twig');
+	}
 }
