36f9597152079431e614f614ae191101fd5bdddf
akorkot <ayoub.korkot@gmail.com>
2017-03-15 21:19:46 +0000

    Doctrine - Relation One-To-One

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -4,156 +4,207 @@
 
 namespace OC\PlatformBundle\Controller;
 
+use OC\PlatformBundle\Entity\Advert;
+use OC\PlatformBundle\Entity\Image;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
 {
-	/**
-	 * @param $page
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 */
-	public function indexAction($page)
-	{
+    /**
+     * @param $page
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction($page)
+    {
+        // Test du service antispam
+        $antispam = $this->container->get('oc_platform.antispam');
+        $text = '...';
+        if ($antispam->isSpam($text)) {
+            //throw new \Exception('Votre message a été détecté comme spam !');
+        }
 
-		// Test du service antispam
-		$antispam = $this->container->get('oc_platform.antispam');
-		$text = '...';
-		if ($antispam->isSpam($text)) {
-			//throw new \Exception('Votre message a été détecté comme spam !');
-		}
 
+        // On ne sait pas combien de pages il y a
+        // Mais on sait qu'une page doit être supérieure ou égale à 1
+        if ($page < 1) {
+            // On déclenche une exception NotFoundHttpException, cela va afficher
+            // une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
+            throw new NotFoundHttpException('Page "' . $page . '" inexistante.');
+        }
 
-		// On ne sait pas combien de pages il y a
-		// Mais on sait qu'une page doit être supérieure ou égale à 1
-		if ($page < 1) {
-			// On déclenche une exception NotFoundHttpException, cela va afficher
-			// une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
-			throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
-		}
-
-		// Ici, on récupérera la liste des annonces, puis on la passera au template
-		// Notre liste d'annonce en dur
-		$listAdverts = array(
-			array(
-				'title'   => 'Recherche développpeur Symfony2',
-				'id'      => 1,
-				'author'  => 'Alexandre',
-				'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
-				'date'    => new \Datetime()),
-			array(
-				'title'   => 'Mission de webmaster',
-				'id'      => 2,
-				'author'  => 'Hugo',
-				'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
-				'date'    => new \Datetime()),
-			array(
-				'title'   => 'Offre de stage webdesigner',
-				'id'      => 3,
-				'author'  => 'Mathieu',
-				'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
-				'date'    => new \Datetime())
-		);
-
-		// Mais pour l'instant, on ne fait qu'appeler le template
-		return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
-			'listAdverts' => $listAdverts
-		));
-	}
-
-	/**
-	 * @param $limit
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 */
-	public function menuAction($limit)
-	{
-		// On fixe en dur une liste ici, bien entendu par la suite
-		// on la récupérera depuis la BDD !
-		$listAdverts = array(
-			array('id' => 2, 'title' => 'Recherche développeur Symfony2'),
-			array('id' => 5, 'title' => 'Mission de webmaster'),
-			array('id' => 9, 'title' => 'Offre de stage webdesigner')
-		);
-
-		return $this->render('OCPlatformBundle:Advert:menu.html.twig', array(
-			// Tout l'intérêt est ici : le contrôleur passe
-			// les variables nécessaires au template !
-			'listAdverts' => $listAdverts
-		));
-	}
-
-	/**
-	 * @param $id
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 */
-	public function viewAction($id)
-	{
-        $advert = array(
-            'title'   => 'Recherche développpeur Symfony2',
-            'id'      => $id,
-            'author'  => 'Alexandre',
-            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
-            'date'    => new \Datetime()
+        // Ici, on récupérera la liste des annonces, puis on la passera au template
+        // Notre liste d'annonce en dur
+        $listAdverts = array(
+            array(
+                'title' => 'Recherche développpeur Symfony2',
+                'id' => 1,
+                'author' => 'Alexandre',
+                'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+                'date' => new \Datetime()),
+            array(
+                'title' => 'Mission de webmaster',
+                'id' => 2,
+                'author' => 'Hugo',
+                'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                'date' => new \Datetime()),
+            array(
+                'title' => 'Offre de stage webdesigner',
+                'id' => 3,
+                'author' => 'Mathieu',
+                'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                'date' => new \Datetime())
         );
 
+        // Mais pour l'instant, on ne fait qu'appeler le template
+        return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+            'listAdverts' => $listAdverts
+        ));
+    }
+
+    /**
+     * @param $limit
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function menuAction($limit)
+    {
+        // On fixe en dur une liste ici, bien entendu par la suite
+        // on la récupérera depuis la BDD !
+        $listAdverts = array(
+            array('id' => 2, 'title' => 'Recherche développeur Symfony2'),
+            array('id' => 5, 'title' => 'Mission de webmaster'),
+            array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
+
+        return $this->render('OCPlatformBundle:Advert:menu.html.twig', array(
+            // Tout l'intérêt est ici : le contrôleur passe
+            // les variables nécessaires au template !
+            'listAdverts' => $listAdverts
+        ));
+    }
+
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function viewAction($id)
+    {
+        // On récupère le repository
+        $repository = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('OCPlatformBundle:Advert');
+
+        // On récupère l'entité correspondante à l'id $id
+        $advert = $repository->find($id);
+
+        // $advert est donc une instance de OC\PlatformBundle\Entity\Advert
+        // ou null si l'id $id  n'existe pas, d'où ce if :
+        if (null === $advert) {
+            throw new NotFoundHttpException("L'annonce d'id " . $id . " n'existe pas.");
+        }
+
+        // Le render ne change pas, on passait avant un tableau, maintenant un objet
         return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
             'advert' => $advert
         ));
-	}
+    }
 
-	/**
-	 * @param Request $request
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-	 */
-	public function addAction(Request $request)
-	{
-		// La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function addAction(Request $request)
+    {
+        // Création de l'entité
+        $advert = new Advert();
+        $advert->setTitle('Recherche développeur Symfony2.');
+        $advert->setAuthor('Alexandre');
+        $advert->setContent("Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…");
+        // On peut ne pas définir ni la date ni la publication,
+        // car ces attributs sont définis automatiquement dans le constructeur
 
-		// Si la requête est en POST, c'est que le visiteur a soumis le formulaire
-		if ($request->isMethod('POST')) {
-			// Ici, on s'occupera de la création et de la gestion du formulaire
+        // Création de l'entité Image
+        $image = new Image();
+        $image->setUrl('http://sdz-upload.s3.amazonaws.com/prod/upload/job-de-reve.jpg');
+        $image->setAlt('Job de rêve');
 
-			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+        // On lie l'image à l'annonce
+        $advert->setImage($image);
 
-			// Puis on redirige vers la page de visualisation de cettte annonce
-			return $this->redirectToRoute('oc_platform_view', array('id' => 5));
-		}
+        // On récupère l'EntityManager
+        $em = $this->getDoctrine()->getManager();
 
-		// Si on n'est pas en POST, alors on affiche le formulaire
-		return $this->render('OCPlatformBundle:Advert:add.html.twig');
-	}
+        // Étape 1 : On « persiste » l'entité
+        $em->persist($advert);
 
-	/**
-	 * @param $id
-	 * @param Request $request
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-	 */
-	public function editAction($id, Request $request)
-	{
+        // Étape 2 : On « flush » tout ce qui a été persisté avant
+        $em->flush();
+
+        // Reste de la méthode qu'on avait déjà écrit
+        if ($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+            return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
+        }
+
+        return $this->render('OCPlatformBundle:Advert:add.html.twig');
+    }
+
+    /**
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function editAction($id, Request $request)
+    {
         $advert = array(
-            'title'   => 'Recherche développpeur Symfony2',
-            'id'      => $id,
-            'author'  => 'Alexandre',
+            'title' => 'Recherche développpeur Symfony2',
+            'id' => $id,
+            'author' => 'Alexandre',
             'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
-            'date'    => new \Datetime()
+            'date' => new \Datetime()
         );
 
         return $this->render('OCPlatformBundle:Advert:edit.html.twig', array(
             'advert' => $advert
         ));
-	}
+    }
 
-	/**
-	 * @param $id
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 */
-	public function deleteAction($id)
-	{
-		// Ici, on récupérera l'annonce correspondant à $id
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteAction($id)
+    {
+        // Ici, on récupérera l'annonce correspondant à $id
 
-		// Ici, on gérera la suppression de l'annonce en question
+        // Ici, on gérera la suppression de l'annonce en question
 
-		return $this->render('OCPlatformBundle:Advert:delete.html.twig');
-	}
+        return $this->render('OCPlatformBundle:Advert:delete.html.twig');
+    }
+
+
+    /**
+     * @param $advertId
+     * @return Response
+     */
+    public function editImageAction($advertId)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        // On récupère l'annonce
+        $advert = $em->getRepository('OCPlatformBundle:Advert')->find($advertId);
+
+        // On modifie l'URL de l'image par exemple
+        $advert->getImage()->setUrl('test.png');
+
+        // On n'a pas besoin de persister l'annonce ni l'image.
+        // Rappelez-vous, ces entités sont automatiquement persistées car
+        // on les a récupérées depuis Doctrine lui-même
+
+        // On déclenche la modification
+        $em->flush();
+
+        return new Response('OK');
+    }
 }
